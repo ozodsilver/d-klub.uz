16f511f765870c16598bcd599ac19708d0a223cd
ozodsilver <ozodsilver@gmail.com>
2022-09-10 11:39:47 +0500

    uzgarishhh

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,8 @@ import Vacansy from './components/vacansy.vue'
 
 <template >
 
+<div class="dd" style="display: none !important;">
+
 
   <HeaderComp  />
 <MainComp></MainComp>
@@ -24,8 +26,19 @@ import Vacansy from './components/vacansy.vue'
 <Vacansy></Vacansy>
 <Footer></Footer>
 
-
+</div>
   
+
+<div class="containerr">
+			<div class="wrapper">
+				<div class="content">
+					<h1>Tez kunda</h1>
+					<p>Dasturchilar klubidan "Mega yangilik"<br>Bizga qo'shiling, kelajagingizni IT bilan quring</p>
+					<!-- <a href="#" class="btn1">Subscribe</a> -->
+					<a href="tg://resolve?domain=<dasturchilarklubi_HR>" class="btn2">Telegram</a>
+				</div> <!-- content ends -->
+			</div> <!-- wrapper ends -->
+		</div> <!-- container ends -->
 
 
 </template>
@@ -63,7 +76,10 @@ import Vacansy from './components/vacansy.vue'
 
 .dark{
   background: rgb(44, 43, 43) !important;
+
   color: white !important;
+
+ 
 }
 
 
@@ -74,4 +90,164 @@ display: none;
 
 @import url('https://fonts.googleapis.com/css2?family=Titan+One&display=swap');
 
+
+
+
+
+
+
+
+
+
+
+
+// cominf aoo
+
+
+
+
+
+
+@import url("https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700");
+*{
+	font-family: 'Open Sans', sans-serif;
+	font-weight: 400;
+}
+body{
+	margin: 0;
+	padding: 0;
+	font-size: 16px;
+}
+img{
+	max-width: 100%;
+}
+.containerr{
+	height: 100vh;
+	width: 100%;
+	background-image:url("./assets/dk1.png");
+//	background-image:url("");
+	background-size: cover;
+	background-position: 50% 0%;
+	/*overflow: hidden;*/
+}
+.wrapper{
+	width: 100%;
+    height: 100%;
+	opacity: 0.7;
+	background: linear-gradient(-55deg, transparent 25%, #16181E 25%, #16181E 75%, transparent 75%, transparent 100%);
+	/*background: linear-gradient(-210deg, transparent 0%, #16181E 0%, #16181E 70%, transparent 50%, transparent 100%);*/
+	transition: all 0.5s cubic-bezier(0.67, 0, 0.3, 1) 1s;
+	display: flex;
+	justify-content:center;
+	align-items:center;
+	text-align: center;
+
+  &:before{
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-image: url('./assets/dk1.png') !important;
+    background-size:  50%;
+    background-position: 130% 0%;
+    background-repeat: no-repeat;
+  }
+
+  &:after{
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-image: url('./assets/dk1.png') !important;
+    background-size:  50%;
+    background-position: -30% 80%;
+    background-repeat: no-repeat;
+  }
+}
+.content{
+	/*transform:rotate(50deg);*/
+	padding: 1rem;
+	color:rgba(255,255,255,1);
+  position: relative;
+  z-index: 999999;
+}
+.content h1{
+	font-size: 3rem;
+	color:rgba(255,255,255,1);
+}
+.content p{
+	font-size: 1.1rem;
+	color:rgba(255,255,255,1);
+}
+.content a{
+	color:rgba(255,255,255,1);
+	display: inline-block;
+	padding: 2.1% 4%;
+    overflow: hidden;
+    border-radius: 0;
+    text-decoration: none;
+    
+}
+.btn1{
+	border: 1px solid rgba(255,255,255,1);;
+	transition: .2s ease-in-out;
+}
+.btn1:hover{
+	border: 1px solid rgba(255,255,255,1);;
+	background-color:rgba(255,255,255,1);;
+	color: black;
+	transition: .2s ease-in-out;
+}
+.btn2{
+	margin-left: 3%;
+    background: linear-gradient(to right, #252AFF 0%, #25FFED 100%);
+    border-image: linear-gradient(to bottom right, #252AFF 0%, #25FFED 100%);
+	border-image-slice: 1;
+	border-width: 1px;
+    border-style: solid;
+    transition: .2s ease-in-out
+}
+.btn2:hover{
+	background: none;
+	border-image: linear-gradient(to bottom right, #252AFF 0%, #25FFED 100%);
+	border-image-slice: 1;
+	border-width: 1px;
+    border-style: solid;
+    transition: .2s ease-in-out
+}
+
+@media screen and (max-width: 355px){
+	.content{
+		/*transform:rotate(50deg);*/
+		padding: 1rem 1.4rem;
+	}
+	.content h1{
+		font-size: 2.5rem;
+		/*margin: 5px 0px;*/
+		/*line-height: 1.2;*/
+	}
+	.wrapper{
+		background: linear-gradient(-210deg, transparent 0%, #16181E 0%, #16181E 70%, transparent 50%, transparent 100%);
+		transition: all 0.5s cubic-bezier(0.67, 0, 0.3, 1) 1s;
+		display: flex;
+		justify-content:center;
+		align-items:flex-start;
+		text-align: left;
+	}
+}
+
+@media screen and (min-width: 356px) and (max-width: 650px){
+	.content{
+		/*transform:rotate(50deg);*/
+		padding: 1rem 1.4rem;
+	}
+	.wrapper{
+		background: linear-gradient(-210deg, transparent 0%, #16181E 0%, #16181E 70%, transparent 50%, transparent 100%);
+		transition: all 0.5s cubic-bezier(0.67, 0, 0.3, 1) 1s;
+		display: flex;
+		justify-content:center;
+		align-items:flex-start;
+		text-align: left;
+	}
+}
 </style>
